ee91cdec46042d8c2e7755ed854eafcd56ff41f2
cyberblunt <75576903+cyberblunt@users.noreply.github.com>
2025-05-16 06:09:46 +0400

    Update AppNavBar.js

--- client/src/components/AppNavBar.js
+++ client/src/components/AppNavBar.js
@@ -1,1 +1,61 @@
 // Этот код представляет собой компонент AppNavBar - навигационную панель приложения для каталога фильмов
+import React, { Component } from 'react';
+import {
+	Container,
+	Navbar,
+	NavbarBrand,
+	NavbarToggler,
+	Nav,
+	NavItem,
+	Collapse
+} from 'reactstrap';
+
+import AddMovieModal from './AddMovieModal';
+import FileUploadModal from './FileUploadModal';
+
+class AppNavBar extends Component {
+	state = {
+		isOpen: false
+	};
+
+	toggle = () => {
+		this.setState({
+			isOpen: !this.state.isOpen
+		});
+	};
+
+	render() {
+		return (
+			<Navbar
+				color="dark"
+				dark
+				expand="sm">
+				<Container>
+					<NavbarBrand
+						className="font-weight-bold"
+						href="/">
+						Movie Catalog
+					</NavbarBrand>
+					<NavbarToggler
+						onClick={ this.toggle }/>
+					<Collapse
+						isOpen={ this.state.isOpen }
+						navbar>
+						<Nav
+							className="ml-auto"
+							navbar>
+							<NavItem>
+								<AddMovieModal/>
+							</NavItem>
+							<NavItem>
+								<FileUploadModal/>
+							</NavItem>
+						</Nav>
+					</Collapse>
+				</Container>
+			</Navbar>
+		);
+	}
+}
+
+export default AppNavBar;
