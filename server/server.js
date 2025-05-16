652cdab3158bb4451dcc2bf360f145d103a0a4a0
cyberblunt <75576903+cyberblunt@users.noreply.github.com>
2025-05-16 04:48:00 +0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,34 @@
 // Этот код представляет собой основной файл приложения (app.js) для веб-сервиса по управлению фильмами. Он настраивает Express-сервер, подключает middleware, устанавливает соединение с MongoDB и определяет роуты.
+
+const config = require('config');
+
+const log = require('debug')('app:log');
+
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const fileUpload = require('express-fileupload');
+const movies = require('./routes/movies');
+
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect(config.get('database.uri'), { useNewUrlParser: true })
+	.then(() => log(`Successfully connected to MongoDB database on "${config.get('database.uri')}"`))
+	.catch((error) => log(`Cannot connect to MongoDB: ${error}`));
+
+app.use(helmet());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload());
+
+app.use(morgan('tiny', { stream: { write: msg => log(msg) } }));
+
+app.use('/api/movies/', movies);
+
+app.listen(config.get('server.port'), config.get('server.host'),
+	() => log(`Server is running on ${config.get('server.host')}:${config.get('server.port')}`));
+
+module.exports = app;
