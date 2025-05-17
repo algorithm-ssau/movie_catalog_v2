493dd220f6072af921ef878e5cab7cebac88df91
Shchegolev <nazarshow69@gmail.com>
2025-05-17 19:01:22 +0400

    Документация - test/movies.js

--- server/test/movies.js
+++ server/test/movies.js
@@ -1,67 +1,74 @@
 // Этот код представляет собой набор тестов для API работы с фильмами, написанный с использованием Chai и Chai HTTP. Он проверяет корректность работы GET и POST запросов к /api/movies/.
 
-let { Movie } = require('../model/movie');
+// Импорт необходимых модулей
+let { Movie } = require('../model/movie'); // Модель Movie из Mongoose
+let chai = require('chai'); // Библиотека для assertions
+let chaiHttp = require('chai-http'); // Плагин для тестирования HTTP-запросов
+let server = require('../server'); // Серверное приложение
+let should = chai.should(); // Стиль assertions через should
 
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('../server');
-let should = chai.should();
+chai.use(chaiHttp); // Подключаем chai-http плагин к chai
 
-chai.use(chaiHttp);
-
+// Основной блок тестов для работы с фильмами
 describe('Movies', () => {
 
-	beforeEach((done) => {
-		Movie.deleteMany({}, (error) => {
-			done();
-		});
-	});
+    // Перед каждым тестом очищаем коллекцию Movie в базе данных
+    beforeEach((done) => {
+        Movie.deleteMany({}, (error) => {
+            done();
+        });
+    });
 
-	describe('/GET /api/movies/', () => {
-		it('It should GET all the movies', (done) => {
-			chai.request(server)
-				.get('/api/movies/')
-				.end((error, response) => {
-					response.should.have.status(200);
-					response.body.should.be.a('array');
-					response.body.length.should.be.eql(0);
-					done();
-				});
-		});
-	});
+    // Тесты для GET /api/movies/ - получение всех фильмов
+    describe('/GET /api/movies/', () => {
+        it('It should GET all the movies', (done) => {
+            chai.request(server)
+                .get('/api/movies/')
+                .end((error, response) => {
+                    response.should.have.status(200); // Проверяем код ответа
+                    response.body.should.be.a('array'); // Ответ должен быть массивом
+                    response.body.length.should.be.eql(0); // Массив должен быть пустым (так как база очищена)
+                    done();
+                });
+        });
+    });
 
-	describe('/POST /api/movies/', () => {
-		it('It should POST a movie ', (done) => {
-			let movie = {
-				title: 'The Girl with Dragon Tattoo',
-				year: 2011,
-				format: 'Blu-Ray',
-				stars: [
-					"Daniel Craig",
-					"Rooney Mara",
-					"Christopher Plummer"
-				]
-			};
+    // Тесты для POST /api/movies/ - создание нового фильма
+    describe('/POST /api/movies/', () => {
+        it('It should POST a movie ', (done) => {
+            // Тестовые данные фильма
+            let movie = {
+                title: 'The Girl with Dragon Tattoo',
+                year: 2011,
+                format: 'Blu-Ray',
+                stars: [
+                    "Daniel Craig",
+                    "Rooney Mara",
+                    "Christopher Plummer"
+                ]
+            };
 
-			chai.request(server)
-				.post('/api/movies/')
-				.send(movie)
-				.end((error, response) => {
-					response.should.have.status(200);
-					response.body.should.be.a('object');
-					response.body.should.have.property('_id');
-					response.body.should.have.property('title');
-					response.body.should.have.property('year');
-					response.body.should.have.property('format');
-					response.body.should.have.property('stars');
-					done();
-				});
-		});
-	});
+            chai.request(server)
+                .post('/api/movies/')
+                .send(movie) // Отправляем данные фильма
+                .end((error, response) => {
+                    response.should.have.status(200); // Проверяем код ответа
+                    response.body.should.be.a('object'); // Ответ должен быть объектом
+                    // Проверяем наличие обязательных полей в ответе
+                    response.body.should.have.property('_id');
+                    response.body.should.have.property('title');
+                    response.body.should.have.property('year');
+                    response.body.should.have.property('format');
+                    response.body.should.have.property('stars');
+                    done();
+                });
+        });
+    });
 
-	afterEach((done) => {
-		Movie.deleteMany({}, (error) => {
-			done();
-		});
-	});
+    // После каждого теста снова очищаем коллекцию Movie
+    afterEach((done) => {
+        Movie.deleteMany({}, (error) => {
+            done();
+        });
+    });
 });
