ffa17fd2d26a2d1ba040f413a6d41e2c8ea527db
MaWlex999 <MaWlex999@yandex.ru>
2025-05-17 19:48:21 +0400

    Update index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,1 +1,7 @@
 // Этот код создает корневой редюсер (root reducer) для Redux-приложения, используя функцию combineReducers из библиотеки Redux
+import { combineReducers } from 'redux';
+import movieReducer from './movieReducer';
+
+export default combineReducers({
+	movie: movieReducer
+});
