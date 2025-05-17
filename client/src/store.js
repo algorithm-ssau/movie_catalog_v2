b37490a222e61bf5f9b92e0cc65b37515c3f301a
MaWlex999 <MaWlex999@yandex.ru>
2025-05-17 19:37:06 +0400

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,1 +1,18 @@
 // Этот код создает Redux-хранилище (store) — централизованное место для хранения состояния приложения.
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+	rootReducer,
+	initialState,
+	compose(
+		applyMiddleware(...middleware)
+	)
+);
+
+export default store;
