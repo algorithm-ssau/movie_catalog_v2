2c3b975e6d94cd93db30bdb06437dd1505dc81e9
MaWlex999 <MaWlex999@yandex.ru>
2025-05-17 20:29:27 +0400

    Документация - index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,18 @@
 // Этот код представляет собой основной входной файл (обычно index.js) для React-приложения, созданного с помощью Create React App (CRA)
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Импортируем необходимые библиотеки
+import React from 'react'; // Основная библиотека React
+import ReactDOM from 'react-dom'; // Для работы с DOM
+import App from './App'; // Главный компонент приложения
+import * as serviceWorker from './serviceWorker'; // Сервис-воркер для PWA
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Рендерим главный компонент App в DOM-элемент с id="root"
+ReactDOM.render(
+  <App />, // Корневой компонент приложения
+  document.getElementById('root') // DOM-контейнер, куда монтируется приложение
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// Настройка сервис-воркера (для PWA-функциональности)
+// По умолчанию сервис-воркер отключен (unregister())
+// Чтобы включить PWA-функции (оффлайн-режим и кэширование), замените unregister() на register()
+// Важно: это имеет некоторые особенности - подробнее: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
